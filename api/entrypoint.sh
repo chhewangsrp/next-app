43612d0df5a7e63b36d77d80189ee233cf0bd0ac
Chhewang Sherpa <chhewangsrp@gmail.com>
2024-01-10 17:46:36 -0700

    Revert "Change baseline to spring boot framework"
    This reverts commit 82db6808b8fb61e71f09f39e59d058eed4a39a6f.

---
+++ api/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "waiting for postgres..."
+
+while ! nc -z database 5432; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+# Apply migrations, create database tables
+if ["$RUN_SETUP_DJANGO" = "true"]; then
+    python manage.py migrate
+fi
+
+exec "$@"
